d78b88cb037f42dc6986e8e83237a4df4e8fad6e
nawelGl <ghazal.nawel@gmail.com>
2026-01-03 23:56:44 +0100

    citations-api creation

---
+++ citations-api/src/main/java/org/gso/citations/CitationsApiApplication.java
@@ -0,0 +1,13 @@
+package org.gso.citations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitationsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitationsApiApplication.class, args);
+	}
+
+}
---
+++ citations-api/src/test/java/org/gso/citations/CitationsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gso.citations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitationsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
