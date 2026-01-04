3cfef1a94e65b797b8cc4fff1d8af0dd1bfda429
nawelGl <ghazal.nawel@gmail.com>
2026-01-04 01:25:52 +0100

    Create SecurityConfiguration.java

---
+++ citations-api/src/main/java/org/gso/citations/configuration/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package org.gso.citations.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {
+                }));
+
+        return http.build();
+    }
+}
