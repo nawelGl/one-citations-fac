d14847629df9ef6a4512b1631f13b46692ef2c00
nawelGl <ghazal.nawel@gmail.com>
2026-01-10 17:09:13 +0100

    Ajout de l'annotation pour le bouton authorize sur Swagger

--- citations-api/src/main/java/org/gso/citations/CitationsApiApplication.java
+++ citations-api/src/main/java/org/gso/citations/CitationsApiApplication.java
@@ -2,8 +2,27 @@ package org.gso.citations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.OAuthScope;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @SpringBootApplication
+@SecurityScheme(
+    name = "Keycloak",
+    type = SecuritySchemeType.OAUTH2,
+    flows = @OAuthFlows(
+        authorizationCode = @OAuthFlow(
+            authorizationUrl = "http://localhost:8080/realms/one-citations/protocol/openid-connect/auth",
+            tokenUrl = "http://localhost:8080/realms/one-citations/protocol/openid-connect/token",
+            scopes = {
+                @OAuthScope(name = "profile", description = "access profile"),
+                @OAuthScope(name = "email", description = "access email")
+            }
+        )
+    )
+)
 public class CitationsApiApplication {
 
 	public static void main(String[] args) {
